7c0619ed7efd62cfd241120e6513665788b1afa6
John Mulligan <phlogistonjohn@asynchrono.us>
2011-01-10 13:37:00 -0500

    name the library "workers"
    Should have checked that the "workerpool" name wasn't already taken on pypi.
    Such is life.

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ except:
     from setuptools import setup
 
 setup(
-    name = "WorkerPool",
+    name = "workers",
     version = "0.1",
     scripts = [],
-    packages = ['workerpool',],
+    packages = ['workers',],
 
     # metadata for upload to PyPI
     author = "Jesse Noller",
     author_email = "jnoller@gmail.com",
-    description = "Workerpool for threads!",
+    description = "Workers, a work pool library for threads!",
     #long_description=open('README.txt').read(),
     license = "Apache Software License 2.0",
     keywords = "threads threadpool parallel",
-    url = "http://opensource.nasuni.com/bender",
+    url = "http://opensource.nasuni.com/workers",
 )
 
--- tests/test_workerpool.py
+++ tests/test_workerpool.py
@@ -1,5 +1,5 @@
 """
-workerpool unit tests
+workers unit tests
 
 Copyright (c) 2010 Nasuni Corporation http://www.nasuni.com/
 
@@ -20,7 +20,7 @@ import threading
 import time
 import unittest
 
-import workerpool
+import workers
 
 
 def _tfunc(val, blah=None):
@@ -57,7 +57,7 @@ class TestWorker(unittest.TestCase):
         self.stop = threading.Event()
 
     def test_worker_basic(self):
-        x = workerpool.Worker(self.run, self.stop, self.inbox,
+        x = workers.Worker(self.run, self.stop, self.inbox,
                               self.outbox, self.errbox, name='flob',
                               stagger=0.1)
         x.start()
@@ -72,7 +72,7 @@ class TestWorker(unittest.TestCase):
         x.join()
 
     def test_worker_err(self):
-        x = workerpool.Worker(self.run, self.stop, self.inbox,
+        x = workers.Worker(self.run, self.stop, self.inbox,
                               self.outbox, self.errbox, name='flob')
         x.start()
         self.run.set()
@@ -91,7 +91,7 @@ class TestWorker(unittest.TestCase):
         def do_work(v, worker_data):
             worker_data['called'] += 1
             return v
-        x = workerpool.Worker(self.run, self.stop, self.inbox,
+        x = workers.Worker(self.run, self.stop, self.inbox,
                               self.outbox, self.errbox,
                               name='worker_with_data', worker_setup=setup)
         x.start()
@@ -114,7 +114,7 @@ class TestMisc(unittest.TestCase):
         q = Queue.Queue()
         for _ in range(100):
             q.put(1)
-        self.assertEqual(sum([i for i in workerpool.iterqueue(q)]), 100)
+        self.assertEqual(sum([i for i in workers.iterqueue(q)]), 100)
 
 
 class TestWorkerThreadPool(unittest.TestCase):
@@ -125,18 +125,18 @@ class TestWorkerThreadPool(unittest.TestCase):
     def test_basic_work(self):
         """ test the basic spawn, work, result cycle
         """
-        x = workerpool.WorkerPool(self.wc)
+        x = workers.WorkerPool(self.wc)
         for i in range(self.wc):
             x.inbox.put((_tfunc, [1], {}))
         x.exile()
-        results = [i for i in workerpool.iterqueue(x.outbox)]
+        results = [i for i in workers.iterqueue(x.outbox)]
         self.assertEqual(len(results), self.wc)
         self.assertEqual(x.name, "WorkerPool-%s" % self.wc)
 
     def test_exceptions(self):
         """ test the exception handling
         """
-        x = workerpool.WorkerPool(self.wc)
+        x = workers.WorkerPool(self.wc)
         for i in range(self.wc):
             x.inbox.put((_err, [1], {}))
         x.exile()
@@ -145,7 +145,7 @@ class TestWorkerThreadPool(unittest.TestCase):
     def test_pool_manip(self):
         """ Test the pool spin up and manipulation methods
         """
-        x = workerpool.WorkerPool(self.wc)
+        x = workers.WorkerPool(self.wc)
         x.run()
         self.assertEqual(x.poolsize(), self.wc)
         x.summon(self.wc)
@@ -160,29 +160,29 @@ class TestWorkerThreadPool(unittest.TestCase):
         the speed at which a task/thread can execute on the queue. This is a
         best effort test.
         """
-        x = workerpool.WorkerPool(self.wc)
+        x = workers.WorkerPool(self.wc)
         for i in range(self.wc * 2):
             x.inbox.put((_lazyfunc, [], {}))
         time.sleep(.6)
         x.pause()
-        results = [i for i in workerpool.iterqueue(x.outbox)]
+        results = [i for i in workers.iterqueue(x.outbox)]
         self.assertEqual(len(results), 10)
         x.run()
         x.exile()
-        results = [i for i in workerpool.iterqueue(x.outbox)]
+        results = [i for i in workers.iterqueue(x.outbox)]
         self.assertEqual(len(results), self.wc)
 
     def test_prodcom(self):
         """ Try making two pools - one of which's outbox is passed in as the
         inbox to a second pool - allows for easy producer/consumer constructs
         """
-        prod = workerpool.WorkerPool(self.wc)
-        com = workerpool.WorkerPool(self.wc, inbox=prod.outbox)
+        prod = workers.WorkerPool(self.wc)
+        com = workers.WorkerPool(self.wc, inbox=prod.outbox)
         for i in range(self.wc):
             prod.inbox.put((_tfunc_return, [1], {}))
         prod.exile()
         com.exile()
-        results = [i for i in workerpool.iterqueue(com.outbox)]
+        results = [i for i in workers.iterqueue(com.outbox)]
         self.assertEqual(len(results), self.wc)
 
     def test_circle(self):
@@ -192,31 +192,31 @@ class TestWorkerThreadPool(unittest.TestCase):
         """
         inbox = Queue.Queue()
         outbox = Queue.Queue()
-        prod = workerpool.WorkerPool(self.wc, inbox=outbox, outbox=inbox)
-        com = workerpool.WorkerPool(self.wc, inbox=inbox, outbox=outbox)
+        prod = workers.WorkerPool(self.wc, inbox=outbox, outbox=inbox)
+        com = workers.WorkerPool(self.wc, inbox=inbox, outbox=outbox)
         for i in range(10):
             inbox.put((_recursive_return, [1], {}))
         com.exile()
         prod.exile()
         results = []
-        results.extend([i for i in workerpool.iterqueue(outbox)])
-        results.extend([i for i in workerpool.iterqueue(inbox)])
+        results.extend([i for i in workers.iterqueue(outbox)])
+        results.extend([i for i in workers.iterqueue(inbox)])
         self.assertEqual(len(results), 10)
 
     def test_pool_ctx(self):
         """ Test the pool contextmanager
         """
-        with workerpool.pool(self.wc) as pool:
+        with workers.pool(self.wc) as pool:
             for i in range(self.wc):
                 pool.inbox.put((_tfunc, [1], {}))
-        results = [i for i in workerpool.iterqueue(pool.outbox)]
+        results = [i for i in workers.iterqueue(pool.outbox)]
         self.assertEqual(len(results), self.wc)
 
     def test_pool_ctx_error(self):
         """ Test the pool contextmanager when an error is raised
         """
         try:
-            with workerpool.pool(self.wc) as pool:
+            with workers.pool(self.wc) as pool:
                 for i in range(self.wc):
                     pool.inbox.put((_tfunc, [1], {}))
                 raise ValueError('foobar')
@@ -224,7 +224,7 @@ class TestWorkerThreadPool(unittest.TestCase):
         except Exception, e:
             err = e
         self.assertEqual(str(err), 'foobar')
-        results = [i for i in workerpool.iterqueue(pool.outbox)]
+        results = [i for i in workers.iterqueue(pool.outbox)]
         self.assertEqual(len(results), self.wc)
 
     def test_pool_with_worker_data(self):
@@ -238,11 +238,11 @@ class TestWorkerThreadPool(unittest.TestCase):
         def do_work(v, worker_data):
             worker_data['called'] += 1
             return v
-        with workerpool.pool(self.wc, worker_setup=setup) as pool:
+        with workers.pool(self.wc, worker_setup=setup) as pool:
             for idx in range(self.wc):
                 pool.inbox.put((do_work, [idx], {}))
             pool.inbox.join()
-        results = [i for i in workerpool.iterqueue(pool.outbox)]
+        results = [i for i in workers.iterqueue(pool.outbox)]
         self.assertEqual(len(results), self.wc)
         self.assertEqual(len(stuff), self.wc)
         self.assertEqual(pool.errbox.qsize(), 0)
@@ -252,7 +252,7 @@ class TestWorkerThreadPool(unittest.TestCase):
         def do_something():
             myname = threading.currentThread().getName()
             names.append(myname)
-        with workerpool.pool(self.wc, suffix='foobly') as pool:
+        with workers.pool(self.wc, suffix='foobly') as pool:
             for _ in range(self.wc):
                 pool.inbox.put((do_something, [], {}))
             pool.inbox.join()
@@ -264,7 +264,7 @@ class TestWorkerThreadPool(unittest.TestCase):
 class TestSummoningPool(unittest.TestCase):
 
     def test_sp_basic(self):
-        x = workerpool.SummoningPool(1, 10, rate=2, ratio=2, interval=0)
+        x = workers.SummoningPool(1, 10, rate=2, ratio=2, interval=0)
         x.pause()
         for _ in range(100):
             x.inbox.put((_lazyfunc, [], {}))
